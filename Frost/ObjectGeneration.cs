f707c8c29fb153424fafe0655cd37823b3ffbb65
Jan Fredrik Bråstad <janfb@uia.no>
2022-04-06 15:30:11 +0200

    fix

---
+++ Frost/ObjectGeneration.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Frost
+{
+    public class ObjectGeneration
+    {
+        public static GameObject Single_obj;
+        public static GameObject Group_obj;
+
+        public void randomObj(UnityEngine.Object Single_obj, int amount, int target_biome, int width, int height)
+        {
+
+            GameObject parent_object;
+
+            parent_object = new GameObject("parent " + Single_obj);
+
+            for (int i = 0; i < amount;)
+            {
+                int xVal = UnityEngine.Random.Range(0, width);
+                int yVal = UnityEngine.Random.Range(0, height);
+
+                if (Setup.obj[xVal, yVal] == target_biome)
+                {
+                    UnityEngine.Object.Instantiate(Single_obj, new Vector3(xVal + 0.5f, yVal + 0.5f, 0), Quaternion.identity, parent_object.transform);
+                    i++;
+                };
+
+            };
+
+
+        }
+
+        private bool restraincheck(int pos_x, int x, int pos_y, int y)
+        {
+            if (pos_x + x > 0 & pos_y + y > 0)
+            {
+                if (pos_x + x < Setup.width & pos_y < Setup.height)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void randomMultiObj(UnityEngine.Object Group_obj, int amount, int target_biome)
+        {
+            GameObject parent_object;
+
+            parent_object = new GameObject("Biome: " + target_biome + " Parent " + Group_obj);
+
+            int areas = 15;
+            int square = 8;
+
+            for (int u = 1; u <= amount;)
+            {
+                int xVal = UnityEngine.Random.Range(0, Setup.width);
+                int yVal = UnityEngine.Random.Range(0, Setup.height);
+                List<List<int>> vectors = new List<List<int>> { };
+                List<int> pos = new List<int> { xVal, yVal };
+                int x, y;
+
+                if (Setup.obj[xVal, yVal] == target_biome)
+                {
+                    u++;
+                    for (int i = 0; i < areas;)
+                    {
+                        x = UnityEngine.Random.Range(-square, square);
+                        y = UnityEngine.Random.Range(-square, square);
+
+                        // vectors.Add(new List<int> { x, y });
+
+
+                        bool check = true;
+
+                        foreach (List<int> v in vectors)
+                        {
+                            if (v[0] == (pos[0] + x) && v[1] == (pos[1] + y))
+                            {
+                                check = false;
+                                break;
+                            }
+                        }
+
+                        if (check)
+                        {
+                            if (restraincheck(pos[0], x, pos[1], y) == true)
+
+                                if (Setup.obj[pos[0] + x, pos[1] + y] == target_biome)
+                                {
+                                    vectors.Add(new List<int> { pos[0] + x, pos[1] + y });
+                                    UnityEngine.Object.Instantiate(Group_obj, new Vector3(pos[0] + x, pos[1] + y, 0), Quaternion.identity, parent_object.transform);
+                                    i++;
+                                }
+                        }
+
+                    }
+                }
+                pos.Clear();
+
+            }
+        }
+
+    }
+}
---
+++ Scripts/Editor Window.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UnityEngine.Tilemaps;
+using SimplexNoise;
+using Frost;
+
+class FrostEditor : EditorWindow
+{
+    public int width, height;
+    public float modifier;
+    public float skip = 0.01f; // find new name
+    public Tile Coast;
+    public Tile Water;
+    public Tile Main_land;
+    public Tile Biome_1;
+    public Tile Biome_2;
+    public Tilemap map;
+    public Object Single_obj;
+    public Object Group_obj;
+    public int amount;
+    public string[] target_biome_array = new string[] {"Coast","Water","Main land","Biome 1", "Biome 2"};
+    public int target_biome;
+    public int index = 0;
+
+    public float waterPercent = 50f;
+    public float beachPercent = 10f;
+
+    Vector2 scrollPos;
+
+
+    [MenuItem("Window/Frost Editor")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(FrostEditor));
+    }
+
+    void convert_target_biome()
+    {
+        switch (index)
+        {
+            case 0:
+                target_biome = 2;
+                break;
+            case 1:
+                target_biome = 3;
+                break;
+            case 2:
+                target_biome = 4;
+                break;
+            case 3:
+                target_biome = 5;
+                break;
+            case 4:
+                target_biome = 6;
+                break;
+            default:
+                Debug.LogError("Unrecognized Option");
+                break;
+        }
+    }
+
+    void OnGUI()
+    {
+        
+        EditorGUIUtility.labelWidth = 100;
+        //EditorGUIUtility.fieldWidth = 20;
+        var label_style = new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter };
+
+
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+        GUILayout.Space(5);
+        GUILayout.Label("Map settings", label_style);
+        GUILayout.Space(3);
+
+        width = EditorGUILayout.IntField("Map width", width);      
+        height = EditorGUILayout.IntField("Map height", height);       
+        map = (Tilemap)EditorGUILayout.ObjectField("Tile Map", map, typeof(Tilemap), true);      
+        waterPercent = EditorGUILayout.FloatField("Water percent", waterPercent);
+        beachPercent = EditorGUILayout.FloatField("Beach percent", beachPercent);
+
+        GUILayout.Space(3);
+        GUILayout.Label("Tiles", label_style);
+        GUILayout.Space(5);
+
+        Coast = (Tile)EditorGUILayout.ObjectField("Coast Tile", Coast, typeof(Tile), true);
+        Water = (Tile)EditorGUILayout.ObjectField("Water Tile", Water, typeof(Tile), true);
+        Main_land = (Tile)EditorGUILayout.ObjectField("Main biome Tile", Main_land, typeof(Tile), true);
+        Biome_1 = (Tile)EditorGUILayout.ObjectField("Biome_1 Tile", Biome_1, typeof(Tile), true);
+        Biome_2 = (Tile)EditorGUILayout.ObjectField("Biome_2 Tile", Biome_2, typeof(Tile), true);
+
+        GUILayout.Space(15);
+        
+        if (GUILayout.Button("Generate Map"))
+        {
+            Debug.Log(width);
+        }
+
+        GUILayout.Space(20);
+        GUILayout.Label("Object generation settings",label_style);
+        GUILayout.Space(5);
+
+        Single_obj = EditorGUILayout.ObjectField("Single object", Single_obj, typeof(Object), true);
+        Group_obj = EditorGUILayout.ObjectField("Grouped objects", Group_obj, typeof(Object), true);
+        amount = EditorGUILayout.IntField("Object amount", amount);
+
+        EditorGUILayout.BeginHorizontal();
+        
+        GUILayout.Label("Target biome", label_style);
+        GUILayout.FlexibleSpace();
+        index = EditorGUILayout.Popup(index, target_biome_array, GUILayout.Width(250));
+        EditorGUILayout.EndHorizontal();
+
+        GUILayout.Space(15);
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Generate object"))
+        {
+            convert_target_biome();
+            
+            Debug.Log(target_biome);
+
+        }
+        GUILayout.Space(3);
+        if (GUILayout.Button("Generate group objects"))
+        {
+           
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndScrollView();
+
+       
+    }
+}
